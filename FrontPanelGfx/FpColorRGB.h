ea9c366db61606d82d59fc79e4d7499087fc7b66
Daniel Zicsi-Liess <liessdaniel415@gmail.com>
2019-12-18 15:12:15 +0100

    added changeable color encoding fature

--- FrontPanelGfx/FpColorRGB.h
+++ FrontPanelGfx/FpColorRGB.h
@@ -8,34 +8,86 @@ namespace fp
 
 using ColorRGB16 = uint16_t;
 
-struct ColorRGB
+
+namespace detail::color
 {
-    ColorRGB() : m_rgb(0xff000000){}
-    ColorRGB(uint32_t r, uint32_t g, uint32_t b) :
-        m_rgb(  (b & 0xff) <<  0 | 
-                (g & 0xff) <<  8 | 
-                (r & 0xff) << 16 |
-                0xff000000 )
+
+class ARGB
+{
+public:
+    constexpr ARGB() noexcept: data(0xff000000) {}
+    constexpr ARGB(uint32_t r, uint32_t g, uint32_t b) noexcept:
+        data(  (b & 0xff) <<  0 | 
+               (g & 0xff) <<  8 | 
+               (r & 0xff) << 16 |
+               0xff000000 )
         {}
-    uint32_t toUint32_0xffRRGGBB() const
-    {
-        return m_rgb;
-    }
-    uint32_t toUint32_0xRRGGBBff() const
-    {
-        return (m_rgb << 8) | 0x000000ff;
-    }
-    uint16_t toUint16_BRG565(uint32_t x, uint32_t y) const;
-    bool operator!=(const ColorRGB& rhs) const
-    {
-        return m_rgb != rhs.m_rgb;
-    }
-    uint8_t r() const { return (m_rgb >> 16) & 0xff; }
-    uint8_t g() const { return (m_rgb >>  8) & 0xff; }
-    uint8_t b() const { return (m_rgb      ) & 0xff; }
+    constexpr uint8_t r() const noexcept{ return (data >> 16) & 0xff; }
+    constexpr uint8_t g() const noexcept{ return (data >>  8) & 0xff; }
+    constexpr uint8_t b() const noexcept{ return (data      ) & 0xff; }
+    constexpr bool operator!=(const ARGB& rhs) const noexcept { return data != rhs.data;}
 private:
-    uint32_t m_rgb; //0xffRRGGBB
+    uint32_t data;
 };
 
+class BGRA
+{
+public:
+    constexpr BGRA() noexcept: data(0x000000ff) {}
+    constexpr BGRA(uint32_t r, uint32_t g, uint32_t b) noexcept: 
+        data(  (b & 0xff) <<  24 | 
+               (g & 0xff) <<  16 | 
+               (r & 0xff) <<  8  |
+               0x000000ff )
+        {}
+    constexpr uint8_t r() const noexcept{ return (data >>  8) & 0xff; }
+    constexpr uint8_t g() const noexcept{ return (data >> 16) & 0xff; }
+    constexpr uint8_t b() const noexcept{ return (data >> 24) & 0xff; }
+    constexpr bool operator!=(const BGRA& rhs) const noexcept { return data != rhs.data;}
+private:
+    uint32_t data;
+};
+
+
+template<typename ColorRepr>
+struct ColorRGB
+{
+    constexpr ColorRGB() noexcept: m_colorRepr() {}
+    constexpr ColorRGB(uint32_t r, uint32_t g, uint32_t b) noexcept: m_colorRepr(r, g, b) {}
+    constexpr uint32_t toUint32_0xffRRGGBB() const noexcept
+    {
+        return 0xff000000 | (r() << 16) | (g() << 8) | b();
+    }
+    constexpr uint32_t toUint32_0xRRGGBBff() const noexcept
+    {
+        return 0x000000ff | (r() << 24) | (g() << 16) | (b() << 8);
+    }
+    constexpr uint16_t toUint16_BRG565_dither(uint32_t x, uint32_t y) const noexcept;
+    constexpr uint16_t toUint16_BRG565() const noexcept
+    {
+        return ((b() >> 3) << 11) | ((g() >> 2) << 5) | (r() >> 3);
+    }
+    constexpr bool operator!=(const ColorRGB& rhs) const noexcept
+    {
+        return m_colorRepr != rhs.m_colorRepr;
+    }
+    constexpr uint8_t r() const noexcept{ return m_colorRepr.r(); }
+    constexpr uint8_t g() const noexcept{ return m_colorRepr.g(); }
+    constexpr uint8_t b() const noexcept{ return m_colorRepr.b(); }
+private:
+    ColorRepr m_colorRepr;
+};
+
+} // namespace detail::color
+
+#ifdef __FP_COLOR_REPRESENTATION_EMBEDDED_STYLE__
+using ColorRGB = detail::color::ColorRGB<detail::color::BGRA>;
+#else
+using ColorRGB = detail::color::ColorRGB<detail::color::ARGB>;
+#endif
+
 } // namespace fp
+
+#include "FpColorRGB.tcc"
+
 #endif
--- FrontPanelGfx/FpColorRGB.tcc
+++ FrontPanelGfx/FpColorRGB.tcc
@@ -37,11 +37,12 @@ static const uint8_t ditherThresholdB[64] = {
     1, 7, 3, 5, 0, 8, 2, 6
 };
 
-uint16_t fp::ColorRGB::toUint16_BRG565(uint32_t x, uint32_t y) const
+template<typename ColorRepr>
+constexpr uint16_t fp::detail::color::ColorRGB<ColorRepr>::toUint16_BRG565_dither(uint32_t x, uint32_t y) const noexcept
 {
     const uint8_t thresholdIndex = ((y & 7) << 3) + (x & 7);
-    uint8_t r = std::min(this->r() + ditherThresholdR[thresholdIndex], 255) >> 3 << 3;
-    uint8_t g = std::min(this->g() + ditherThresholdG[thresholdIndex], 255) >> 2 << 2;
-    uint8_t b = std::min(this->b() + ditherThresholdB[thresholdIndex], 255) >> 3 << 3;
+    uint8_t r = std::min(m_colorRepr.r() + ditherThresholdR[thresholdIndex], 255) >> 3 << 3;
+    uint8_t g = std::min(m_colorRepr.g() + ditherThresholdG[thresholdIndex], 255) >> 2 << 2;
+    uint8_t b = std::min(m_colorRepr.b() + ditherThresholdB[thresholdIndex], 255) >> 3 << 3;
     return ((b >> 3) << 11) | ((g >> 2) << 5) | (r >> 3);
 }
